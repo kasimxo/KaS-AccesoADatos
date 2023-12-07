44ff9ffea391f35c13776775eef0f97a0c82d73e
kasimxo <kasimxo@gmail.com>
2023-12-07 12:30:17 +0100

    Save copy

--- src/ejercicios/CrearEmpleadoXml.java
+++ src/ejercicios/CrearEmpleadoXml.java
@@ -1,10 +1,91 @@
 package ejercicios;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.io.File;
+
 public class CrearEmpleadoXml {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		File f = new File("./src/archivosEjemplo/Empleados.xml");
+		
+		String[] nombres = {"Juan", "Elena", "Pedro", "Lucia", "Myriam", "Eli", "Andres", "Antonio", "Jose Luis"};
+		String[] apellidos = {"Sanchez", "Lopez", "Rodriguez", "Perez", "Gonzalez", "Martinez", "Fernandez", "Gomez", "Hernandez"};
+		String nombre = nombres[(int) (Math.random()*nombres.length)];
+		String apellido = apellidos[(int) (Math.random()*apellidos.length)] + " " + apellidos[(int) (Math.random()*apellidos.length)];
+		int sueldoN = (int) ((Math.random()*20+10)*100);
+		String sueldo = Integer.toString(sueldoN);
+		String edad = Integer.toString((int)(Math.random()*50+20));
+		
+		System.out.println(nombre + apellido + sueldo + edad);
 
+	        try {
+	        	DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	 	        DocumentBuilder db = dbf.newDocumentBuilder();
+
+	 	        // root elements
+	 	        Document doc = db.parse(f);
+	 	        
+	 	        Element rootElement = (Element) doc.getFirstChild();
+	 	        
+	 	        //doc.appendChild(rootElement);
+
+	 	        //doc.createElement("empleado");
+	 	        Element empleado = doc.createElement("empleado");
+	 	        
+	 	        String id = Integer.toString(Integer.valueOf(rootElement.getLastChild().getAttributes().item(0).getNodeValue().toString()) + 1);
+	 	        
+	 	        empleado.setAttribute("id", id);
+	 	        
+	 	        
+	 	        Element nombreNodo = doc.createElement("nombre");
+	 	        nombreNodo.appendChild(doc.createTextNode(nombre));
+	 	        empleado.appendChild(nombreNodo);
+	 	        
+	 	        Element apellidosNodo = doc.createElement("apellidos");
+	 	        apellidosNodo.appendChild(doc.createTextNode(apellido));
+	 	        empleado.appendChild(apellidosNodo);
+	 	        
+	 	        Element edadNodo = doc.createElement("edad");
+	 	        edadNodo.appendChild(doc.createTextNode(edad));
+	 	        empleado.appendChild(edadNodo);
+	 	      
+	 	        Element sueldoNodo = doc.createElement("sueldo");
+	 	        sueldoNodo.appendChild(doc.createTextNode(sueldo));
+	 	        empleado.appendChild(sueldoNodo);
+	 	       	
+	 	       	rootElement.appendChild(empleado);
+	 	       	
+	        	FileOutputStream output = new FileOutputStream(f); 
+	        	
+	            writeXml(doc, output);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+
+	    }
+
+	    // write doc to output stream
+	    private static void writeXml(Document doc, OutputStream output) throws TransformerException {
+
+	        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	        Transformer transformer = transformerFactory.newTransformer();
+	        DOMSource source = new DOMSource(doc);
+	        StreamResult result = new StreamResult(output);
+
+	        transformer.transform(source, result);
+
+	    
 	}
 
 }
--- src/ejercicios/LecturaEmpleadoXml.java
+++ src/ejercicios/LecturaEmpleadoXml.java
@@ -1,9 +1,59 @@
 package ejercicios;
 
+// Importamos la librerías necesarias
+import java.io.File;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 public class LecturaEmpleadoXml {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		File f = new File("./src/archivosEjemplo/Empleados.xml"); // Creamos la variable del archivo xml que vamos a leer
+		
+
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance(); // Creamos la variable de la api para manejar el documento
+
+		try {
+
+			DocumentBuilder db = dbf.newDocumentBuilder(); // Creamos la variable que construye el documento xml
+
+			Document doc = db.parse(f); // Creamos la varibale que va a parsear el documento y le pasamos comoargumento el archivo xml especificado
+			
+			System.out.println("Listado de empleados:\n"); // Mostaramos un mensaje de cabecera
+
+			NodeList list = doc.getElementsByTagName("empleado");
+
+			for (int temp = 0; temp < list.getLength(); temp++) {
+
+				Node node = list.item(temp);
+
+				if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element element = (Element) node;
+
+					String id = element.getAttribute("id");
+
+					String nombre = element.getElementsByTagName("nombre").item(0).getTextContent();
+					String apellidos = element.getElementsByTagName("apellidos").item(0).getTextContent();
+					String edad = element.getElementsByTagName("edad").item(0).getTextContent();
+					String sueldo = element.getElementsByTagName("sueldo").item(0).getTextContent();
+
+					System.out.println("Id de empleado: " + id);
+					System.out.println("Nombre completo: " + nombre + " " + apellidos);
+					System.out.println("Edad: " + edad);
+					System.out.println("Sueldo: " + sueldo);
+					System.out.println();
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
